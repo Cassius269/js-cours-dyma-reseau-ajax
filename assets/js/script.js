3202e7db8ad28a05de860207d9c96cad1303a215
cassius269 <fahamygaston@gmail.com>
2024-08-18 02:24:25 +0200

    Récupération de données en asynchrone à l'aide de la fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("hello");
+// Rechercher une ressource todo à l'ID 1 et affichage de l'objet
+fetch('https://jsonplaceholder.typicode.com/todos/1') // on obtient une promesse
+// .then( response => response.json()) // convertir la promesse en objet JSON
+.then( response => response.json()) // convertir la promesse en objet JSON
+.then( data => {// Traitement de la donnée reccueillie
+    console.log(data);
+})
+.catch(error => {
+    console.log(error);
+})
+
+
+// Création d'une fonction asynchrone de récupération de ressource
+async function getRessource(ressource){
+    try {
+        const promise = await fetch(ressource);
+        const data = await promise.json();
+    
+        console.log(data);
+    } catch (error) {
+        console.error("Erreur lors de la récupération de la ressource", error);
+    }
+   
+}
+
+getRessource('https://jsonplaceholder.typicode.com/users/1');
